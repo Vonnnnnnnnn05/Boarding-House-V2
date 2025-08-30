b8d98420d6c75d14e503610706f5727df22b7feb
Your New GitHub Name <yournewemail@example.com>
2025-08-30 09:16:01 +0800

    Back to original

--- conn.php
+++ conn.php
@@ -1,12 +1,3 @@
 <?php
-$servername = "db";
-$username = "user";
-$password = "password";
-$dbname = "bhv2";
-
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+$conn = mysqli_connect("localhost", "root", "", "bhv2");
 ?>
